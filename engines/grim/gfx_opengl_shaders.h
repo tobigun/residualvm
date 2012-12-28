6555345980c0c2f350d43083885c766f7bcb1bf1
Dries Harnie <dries@harnie.be>
2012-12-28 03:59:14 +0100

    GRAPHICS: CreateModel in GfxBase

--- engines/grim/gfx_opengl_shaders.h
+++ engines/grim/gfx_opengl_shaders.h
@@ -192,7 +192,9 @@ public:
 	void renderZBitmaps(bool render);
 
 	virtual void createSpecialtyTextures();
+
 	virtual void createEMIModel(EMIModel *model);
+	virtual void createModel(Mesh *mesh);
 
 protected:
 	void setupShaders();
